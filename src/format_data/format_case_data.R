fed26559b054fb3b5fba0980ba56ce6c987a2058
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-09-30 16:14:44 +0200

    Format and explore data from CDP

--- src/format_data/format_case_data.R
+++ src/format_data/format_case_data.R
@@ -42,6 +42,24 @@ expect_equal(dim(case_dat)[1], length(unique(case_dat$date)) * length(unique(cas
 
 # ---------------------------------------------------------------------------------------------------------------------
 
+# Load and format Corona Daten Plattform data
+
+# Read in data:
+cdp_dat <- read_csv('data/raw/cdp/infektionen.csv')
+
+# Format data:
+cdp_dat <- cdp_dat %>%
+  filter(variable == 'kr_inf_md') %>%
+  select(-c(`_id`, ags2, bundesland, kreis, variable)) %>%
+  pivot_longer(-ags5, names_to = 'date', values_to = 'cases') %>%
+  mutate(date = as.Date(str_sub(date, 2, 9), format = '%Y%m%d')) %>%
+  rename('lk' = 'ags5')
+
+# Check that all LKs have data for all available dates:
+expect_equal(dim(cdp_dat)[1], length(unique(cdp_dat$date)) * length(unique(cdp_dat$lk)))
+
+# ---------------------------------------------------------------------------------------------------------------------
+
 # Incorporate population data
 
 # Get population data by Landkreis:
@@ -59,19 +77,26 @@ case_dat_new <- case_dat %>%
   left_join(pop_dat, by = 'lk')
 expect_identical(dim(case_dat)[1], dim(case_dat_new)[1])
 
+cdp_dat_new <- cdp_dat %>%
+  left_join(pop_dat, by = 'lk')
+expect_identical(dim(cdp_dat)[1], dim(cdp_dat_new)[1])
+
 case_dat <- case_dat_new
-rm(case_dat_new, pop_dat)
+cdp_dat <- cdp_dat_new
+rm(case_dat_new, cdp_dat_new, pop_dat)
 
 # Calculate rates per 100,000 population:
 case_dat <- case_dat %>%
   mutate(case_rate = cases / pop * 100000, .after = cases)
+cdp_dat <- cdp_dat %>%
+  mutate(case_rate = cases / pop * 100000, .after = cases)
 
-# # Write data to file:
-# write_csv(case_dat, file = 'data/formatted/daily_covid_cases_by_lk_CUMULATIVE.csv')
+# Write data to file:
+write_csv(case_dat, file = 'data/formatted/daily_covid_cases_by_lk_CUMULATIVE.csv')
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-# Get weekly data
+# Get weekly data (crowdsourced data)
 
 # Limit data to only rows at week ends:
 week_ends <- unique(case_dat$date)[format(unique(case_dat$date), '%w') == '0']
@@ -89,8 +114,42 @@ case_dat_wk <- case_dat_wk %>%
          .after = date) %>%
   mutate(Year = if_else(Week == 53, '2020', Year))
 
-# # Write data to file:
-# write_csv(case_dat_wk, file = 'data/formatted/weekly_covid_cases_by_lk_CUMULATIVE.csv')
+# Write data to file:
+write_csv(case_dat_wk, file = 'data/formatted/weekly_covid_cases_by_lk_CUMULATIVE.csv')
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Get weekly data (CDP data)
+
+# Add week and year numbers:
+cdp_dat <- cdp_dat %>%
+  mutate(Week = format(date, '%V'),
+         Year = format(date, '%Y'),
+         Year = if_else(Week == 53, '2020', Year),
+         year_week = paste(Year, Week, sep = '_'))
+
+# Remove incomplete weeks:
+dates_to_remove <- cdp_dat %>%
+  group_by(lk, year_week) %>%
+  summarise(len = length(cases)) %>%
+  filter(len < 7) %>%
+  ungroup() %>%
+  select(-c(lk, len)) %>%
+  unique()
+
+cdp_dat <- cdp_dat %>%
+  filter(!(year_week %in% dates_to_remove$year_week))
+
+# Sum over each LK/week:
+cdp_dat <- cdp_dat %>%
+  group_by(lk, Year, Week, pop) %>%
+  summarise(cases = sum(cases)) %>%
+  mutate(case_rate = cases / pop * 100000) %>%
+  ungroup() %>%
+  select(Year:Week, lk, cases:case_rate, pop)
+
+# Write data to file:
+write_csv(cdp_dat, file = 'data/formatted/weekly_covid_cases_by_lk_CDP.csv')
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -118,8 +177,8 @@ case_inc <- case_inc %>%
 
 expect_equal(dim(case_dat_wk)[1], dim(case_inc)[1])
 
-print(length(case_inc$cases[case_inc$cases < 0])) # 112 negative values
-# mostly small values, but occasionally goes to -633 (at least 6 cases of going below -100)?
+print(length(case_inc$cases[case_inc$cases < 0])) # 182 negative values
+# mostly small values, but occasionally goes to -633 (at least 11 cases of going below -100)?
 
 # Replace negatives with NAs:
 case_inc <- case_inc %>%
@@ -129,8 +188,8 @@ case_inc <- case_inc %>%
 case_inc <- case_inc %>%
   mutate(case_rate = cases / pop * 100000)
 
-# # Write data to file:
-# write_csv(case_inc, file = 'data/formatted/weekly_covid_cases_by_lk_INCIDENT.csv')
+# Write data to file:
+write_csv(case_inc, file = 'data/formatted/weekly_covid_cases_by_lk_INCIDENT.csv')
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -143,11 +202,35 @@ not_strictly_inc <- case_dat_wk %>%
   filter(cases != check) %>%
   pull(lk) %>%
   unique()
-# only 88 - so sometimes occurs multiple times per LK
+# only 121 - so sometimes occurs multiple times per LK
 p1 <- ggplot(case_dat[case_dat$lk %in% not_strictly_inc, ], aes(x = date, y = cases)) +
   geom_line() + facet_wrap(~ lk, scales = 'free_y') + theme_classic() +
   labs(x = 'Date', y = 'Cumulative Cases')
 print(p1)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Compare two data sources
+
+# Join data sets:
+cases_all <- case_inc %>%
+  left_join(cdp_dat, by = c('lk', 'Year', 'Week'))
+expect_true(all.equal(cases_all$pop.x, cases_all$pop.y))
+
+# Format:
+cases_all <- cases_all %>%
+  select(date, lk, cases.x, cases.y) %>%
+  rename('cases_cs' = 'cases.x',
+         'cases_cdp' = 'cases.y') %>%
+  pivot_longer(cases_cs:cases_cdp,
+               names_to = 'Source',
+               values_to = 'cases')
+
+# Plot:
+p2 <- ggplot(data = cases_all, aes(x = date, y = cases, col = Source, lty = Source)) +
+  geom_line() + facet_wrap(~ lk, scales = 'free_y') + theme_classic()
+# print(p2)
+# Not exactly the same, but very similar
 
 # ---------------------------------------------------------------------------------------------------------------------
 
--- src/format_data/format_mortality_data.R
+++ src/format_data/format_mortality_data.R
@@ -42,6 +42,24 @@ expect_equal(dim(mortality_dat)[1], length(unique(mortality_dat$date)) * length(
 
 # ---------------------------------------------------------------------------------------------------------------------
 
+# Load and format Corona Daten Plattform data
+
+# Read in data:
+cdp_dat <- read_csv('data/raw/cdp/todesfaelle.csv')
+
+# Format data:
+cdp_dat <- cdp_dat %>%
+  filter(variable == 'kr_tod_md') %>%
+  select(-c(`_id`, ags2, bundesland, kreis, variable)) %>%
+  pivot_longer(-ags5, names_to = 'date', values_to = 'deaths') %>%
+  mutate(date = as.Date(str_sub(date, 2, 9), format = '%Y%m%d')) %>%
+  rename('lk' = 'ags5')
+
+# Check that all LKs have data for all available dates:
+expect_equal(dim(cdp_dat)[1], length(unique(cdp_dat$date)) * length(unique(cdp_dat$lk)))
+
+# ---------------------------------------------------------------------------------------------------------------------
+
 # Incorporate population data
 
 # Get population data by Landkreis:
@@ -59,19 +77,24 @@ mortality_dat_new <- mortality_dat %>%
   left_join(pop_dat, by = 'lk')
 expect_identical(dim(mortality_dat)[1], dim(mortality_dat_new)[1])
 
+cdp_dat_new <- cdp_dat %>%
+  left_join(pop_dat, by = 'lk')
+expect_identical(dim(cdp_dat)[1], dim(cdp_dat_new)[1])
+
 mortality_dat <- mortality_dat_new
-rm(mortality_dat_new, pop_dat)
+cdp_dat <- cdp_dat_new
+rm(mortality_dat_new, cdp_dat_new, pop_dat)
 
 # Calculate rates per 100,000 population:
 mortality_dat <- mortality_dat %>%
   mutate(death_rate = deaths / pop * 100000, .after = deaths)
 
-# # Write data to file:
-# write_csv(mortality_dat, file = 'data/formatted/daily_covid_deaths_by_lk_CUMULATIVE.csv')
+# Write data to file:
+write_csv(mortality_dat, file = 'data/formatted/daily_covid_deaths_by_lk_CUMULATIVE.csv')
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-# Get weekly data
+# Get weekly data (crowdsourced data)
 
 # Limit data to only rows at week ends:
 week_ends <- unique(mortality_dat$date)[format(unique(mortality_dat$date), '%w') == '0']
@@ -89,8 +112,42 @@ mortality_dat_wk <- mortality_dat_wk %>%
          .after = date) %>%
   mutate(Year = if_else(Week == 53, '2020', Year))
 
-# # Write data to file:
-# write_csv(mortality_dat_wk, file = 'data/formatted/weekly_covid_deaths_by_lk_CUMULATIVE.csv')
+# Write data to file:
+write_csv(mortality_dat_wk, file = 'data/formatted/weekly_covid_deaths_by_lk_CUMULATIVE.csv')
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Get weekly data (CDP data)
+
+# Add week and year numbers:
+cdp_dat <- cdp_dat %>%
+  mutate(Week = format(date, '%V'),
+         Year = format(date, '%Y'),
+         Year = if_else(Week == 53, '2020', Year),
+         year_week = paste(Year, Week, sep = '_'))
+
+# Remove incomplete weeks:
+dates_to_remove <- cdp_dat %>%
+  group_by(lk, year_week) %>%
+  summarise(len = length(deaths)) %>%
+  filter(len < 7) %>%
+  ungroup() %>%
+  select(-c(lk, len)) %>%
+  unique()
+
+cdp_dat <- cdp_dat %>%
+  filter(!(year_week %in% dates_to_remove$year_week))
+
+# Sum over each LK/week:
+cdp_dat <- cdp_dat %>%
+  group_by(lk, Year, Week, pop) %>%
+  summarise(deaths = sum(deaths)) %>%
+  mutate(death_rate = deaths / pop * 100000) %>%
+  ungroup() %>%
+  select(Year:Week, lk, deaths:death_rate, pop)
+
+# Write data to file:
+write_csv(cdp_dat, file = 'data/formatted/weekly_covid_deaths_by_lk_CDP.csv')
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -116,7 +173,7 @@ expect_true(all.equal(mortality_inc$deaths.x, mortality_inc$deaths.y))
 # Estimates are the same - keep only one data frame:
 mortality_inc <- mortality_inc1
 rm(mortality_inc1, mortality_inc2)
-print(length(mortality_inc$deaths[mortality_inc$deaths < 0])) # 12 negative values
+print(length(mortality_inc$deaths[mortality_inc$deaths < 0])) # 54 negative values
 # no clear geographic pattern to these, and most have abs. val. < 10
 
 # Replace negatives with NAs:
@@ -127,8 +184,8 @@ mortality_inc <- mortality_inc %>%
 mortality_inc <- mortality_inc %>%
   mutate(death_rate = deaths / pop * 100000)
 
-# # Write data to file:
-# write_csv(mortality_inc, file = 'data/formatted/weekly_covid_deaths_by_lk_INCIDENT.csv')
+# Write data to file:
+write_csv(mortality_inc, file = 'data/formatted/weekly_covid_deaths_by_lk_INCIDENT.csv')
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -152,6 +209,32 @@ p2 <- ggplot(mortality_dat[mortality_dat$lk %in% not_strictly_inc, ], aes(x = da
   geom_line() + facet_wrap(~ lk, scales = 'free_y') + theme_classic() +
   labs(x = 'Date', y = 'Cumulative Deaths')
 print(p2)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Compare two data sources
+
+# Join data sets:
+mortality_all <- mortality_inc %>%
+  left_join(cdp_dat, by = c('lk', 'Year', 'Week'))
+expect_true(all.equal(mortality_all$pop.x, mortality_all$pop.y))
+
+# Format:
+mortality_all <- mortality_all %>%
+  select(date, lk, deaths.x, deaths.y) %>%
+  rename('deaths_cs' = 'deaths.x',
+         'deaths_cdp' = 'deaths.y') %>%
+  pivot_longer(deaths_cs:deaths_cdp,
+               names_to = 'Source',
+               values_to = 'deaths')
+
+# Plot:
+p3 <- ggplot(data = mortality_all, aes(x = date, y = deaths, col = Source, lty = Source)) +
+  geom_line() + facet_wrap(~ lk, scales = 'free_y') + theme_classic()
+print(p3)
+# Less similar than case data, but still relatively close; however, it does seem like CDP data tend to rise/peak before
+# the crowdsourced data, as expected if the reporting dates are when the case was initially reported, and not when the
+# deaths itself was reported
 
 # ---------------------------------------------------------------------------------------------------------------------
 
