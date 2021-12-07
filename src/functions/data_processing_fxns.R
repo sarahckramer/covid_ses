c47892e297c58842c21c6a6cb5a718a2e7e72c80
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-12-07 14:14:00 +0100

    Add fxn to reallocate data with no age information

--- src/functions/data_processing_fxns.R
+++ src/functions/data_processing_fxns.R
@@ -84,3 +84,19 @@ convert_to_incident <- function(dat) {
   # Return incident data:
   return(dat_inc)
 }
+
+
+reallocate_preserving_sum <- function(dat_orig, to_add) {
+  # Fxn to reallocate cases with no age info and round, preserving the total number of cases
+  # Adapted from: https://stackoverflow.com/questions/32544646/round-vector-of-numerics-to-integer-while-preserving-their-sum
+  # param dat_orig: Named numeric vector of case counts in each age group
+  # param to_add: Number of cases with no age information
+  # returns: Named numeric vector of cases, with cases w/o age info distributed proportionally
+  
+  dat_update <- dat_orig + to_add * (dat_orig / sum(dat_orig))
+  dat_out <- floor(dat_update)
+  indices <- tail(order(dat_update - dat_out), round(sum(dat_update)) - sum(dat_out))
+  dat_out[indices] <- dat_out[indices] + 1
+  return(dat_out)
+  
+}
