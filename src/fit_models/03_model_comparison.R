68afe7163362be97ea18fac40d8d33fe77488ef8
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2022-01-31 13:48:43 +0100

    Update k values for spatiotemporal component

--- src/fit_models/03_model_comparison.R
+++ src/fit_models/03_model_comparison.R
@@ -28,20 +28,20 @@ n2b_full <- read_rds('results/fitted_models/FULL_n2b_ml.rds')
 # Explore potential model improvements
 
 # Should living_area be included?:
-n1a_comp <- gam(cases_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) + s(ags2, bs = 're', k = 16) +
+n1a_comp <- gam(cases_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 60) + s(ags2, bs = 're', k = 16) +
                   s(perc_18to64) + s(care_home_beds) + s(GISD_Score) + s(pop_dens) +
                   s(perc_service) + s(perc_production) +
                   offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
-n2a_comp <- gam(cases_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 70) + s(ags2, bs = 're', k = 16) +
+n2a_comp <- gam(cases_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) + s(ags2, bs = 're', k = 16) +
                   s(perc_18to64) + s(care_home_beds, k = 25) + s(GISD_Score) + s(pop_dens) +
                   s(perc_service) + s(perc_production) + s(cases_pre_rate) +
                   offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
 
-n1a_comp_alt <- gam(cases_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) + s(ags2, bs = 're', k = 16) +
+n1a_comp_alt <- gam(cases_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 60) + s(ags2, bs = 're', k = 16) +
                       s(perc_18to64) + s(care_home_beds) + s(GISD_Score) + s(living_area, k = 25) +
                       s(perc_service) + s(perc_production) +
                       offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
-n2a_comp_alt <- gam(cases_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 70) + s(ags2, bs = 're', k = 16) +
+n2a_comp_alt <- gam(cases_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) + s(ags2, bs = 're', k = 16) +
                       s(perc_18to64) + s(care_home_beds, k = 25) + s(GISD_Score) + s(living_area) +
                       s(perc_service) + s(perc_production) + s(cases_pre_rate) +
                       offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
@@ -60,15 +60,15 @@ BIC(n2a_full, n2a_comp_alt)
 
 # Try using MRF:
 dat_cumulative$ARS <- factor(dat_cumulative$lk)
-n1a_mrf <- gam(cases_wave1 ~ s(ARS, bs = 'mrf', xt = list(nb = nb), k = 50) + s(ags2, bs = 're', k = 16) +
+n1a_mrf <- gam(cases_wave1 ~ s(ARS, bs = 'mrf', xt = list(nb = nb), k = 60) + s(ags2, bs = 're', k = 16) +
                  s(perc_18to64) + s(care_home_beds) + s(GISD_Score) + s(pop_dens) + s(living_area, k = 25) +
                  s(perc_service) + s(perc_production) +
                  offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
-n1b_mrf <- gam(deaths_wave1 ~ s(ARS, bs = 'mrf', xt = list(nb = nb), k = 60) + s(ags2, bs = 're', k = 16) +
+n1b_mrf <- gam(deaths_wave1 ~ s(ARS, bs = 'mrf', xt = list(nb = nb), k = 40) + s(ags2, bs = 're', k = 16) +
                  s(hosp_beds) + s(care_home_beds) + s(GISD_Score) + s(pop_dens) +
                  offset(log(cases_wave1)), data = dat_cumulative, family = 'nb', method = 'ML')
 
-n2a_mrf <- gam(cases_wave2 ~ s(ARS, bs = 'mrf', xt = list(nb = nb), k = 70) + s(ags2, bs = 're', k = 16) +
+n2a_mrf <- gam(cases_wave2 ~ s(ARS, bs = 'mrf', xt = list(nb = nb), k = 50) + s(ags2, bs = 're', k = 16) +
                  s(perc_18to64) + s(care_home_beds, k = 25) + s(GISD_Score) + s(pop_dens) + s(living_area) +
                  s(perc_service) + s(perc_production) + s(cases_pre_rate) +
                  offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
@@ -89,15 +89,15 @@ AIC(n2b_full, n2b_mrf)
 BIC(n2b_full, n2b_mrf)
 
 # Compare with poisson/zero-inflated:
-n1a_pois <- gam(cases_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) + s(ags2, bs = 're', k = 16) +
+n1a_pois <- gam(cases_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 60) + s(ags2, bs = 're', k = 16) +
                   s(perc_18to64) + s(care_home_beds) + s(GISD_Score) + s(pop_dens) + s(living_area, k = 25) +
                   s(perc_service) + s(perc_production) +
                   offset(log(pop)), data = dat_cumulative, family = 'poisson', method = 'ML')
-n1b_pois <- gam(deaths_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 60) + s(ags2, bs = 're', k = 16) +
+n1b_pois <- gam(deaths_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 40) + s(ags2, bs = 're', k = 16) +
                   s(hosp_beds) + s(care_home_beds) + s(GISD_Score) + s(pop_dens) +
                   offset(log(cases_wave1)), data = dat_cumulative, family = 'poisson', method = 'ML')
 
-n2a_pois <- gam(cases_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 70) + s(ags2, bs = 're', k = 16) +
+n2a_pois <- gam(cases_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) + s(ags2, bs = 're', k = 16) +
                   s(perc_18to64) + s(care_home_beds, k = 25) + s(GISD_Score) + s(pop_dens) + s(living_area) +
                   s(perc_service) + s(perc_production) +
                   s(cases_pre_rate) + offset(log(pop)), data = dat_cumulative, family = 'poisson', method = 'ML')
@@ -105,7 +105,7 @@ n2b_pois <- gam(deaths_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) 
                   s(hosp_beds) + s(care_home_beds) + s(GISD_Score, k = 25) + s(pop_dens) +
                   s(cases_pre_rate) + offset(log(cases_wave2)), data = dat_cumulative, family = 'poisson', method = 'ML')
 
-n1b_zip <- gam(deaths_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 60) + s(ags2, bs = 're', k = 16) +
+n1b_zip <- gam(deaths_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 40) + s(ags2, bs = 're', k = 16) +
                  s(hosp_beds) + s(care_home_beds) + s(GISD_Score) + s(pop_dens) +
                  offset(log(cases_wave1)), data = dat_cumulative, family = 'ziP', method = 'ML')
 
@@ -116,15 +116,15 @@ BIC(n2a_full, n2a_pois)
 BIC(n2b_full, n2b_pois)
 
 # Change random effects:
-n1a_fixed <- gam(cases_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) +
+n1a_fixed <- gam(cases_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 60) +
                    s(perc_18to64) + s(care_home_beds) + s(GISD_Score) + s(pop_dens) + s(living_area, k = 25) +
                    s(perc_service) + s(perc_production) +
                    offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
-n1b_fixed <- gam(deaths_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 60) +
+n1b_fixed <- gam(deaths_wave1 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 40) +
                    s(hosp_beds) + s(care_home_beds) + s(GISD_Score) + s(pop_dens) +
                    offset(log(cases_wave1)), data = dat_cumulative, family = 'nb', method = 'ML')
 
-n2a_fixed <- gam(cases_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 70) +
+n2a_fixed <- gam(cases_wave2 ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 50) +
                    s(perc_18to64) + s(care_home_beds, k = 25) + s(GISD_Score) + s(pop_dens) + s(living_area) +
                    s(perc_service) + s(perc_production) +
                    s(cases_pre_rate) + offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
