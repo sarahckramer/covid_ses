3ac7a79afe6d6302fdfd89c664444b94ac08ccbf
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2022-02-07 12:24:41 +0100

    Update exploratory/plotting files for cumulative data

--- src/explore_data/explore_lag_between_cases_deaths.R
+++ src/explore_data/explore_lag_between_cases_deaths.R
@@ -12,15 +12,10 @@ library(gridExtra)
 
 # Read in and format data
 
-# Read in crowdsourced data:
-dat_inc_wk <- read_csv('data/formatted/weekly_covid_deaths_by_lk_INCIDENT.csv')
-
 # Read in CDP data:
-dat_inc_cdp <- read_csv('data/formatted/weekly_covid_deaths_by_lk_CDP.csv')
+dat_inc_cdp <- read_csv('data/formatted/weekly_covid_deaths_by_lk_INCIDENT_CDP.csv')
 
 # Format:
-dat_inc_wk <- dat_inc_wk %>%
-  mutate(time = if_else(Year == 2020, Week, Week + 53))
 dat_inc_cdp <- dat_inc_cdp %>%
   mutate(Week = as.numeric(Week)) %>%
   mutate(time = if_else(Year == 2020, Week, Week + 53))
@@ -30,23 +25,11 @@ dat_inc_cdp <- dat_inc_cdp %>%
 # Plot and calculate difference:
 
 # Plot data by LK:
-p1 <- ggplot(data = dat_inc_wk, aes(x = time, y = case_rate, group = lk)) + geom_line() + theme_classic()
-p2 <- ggplot(data = dat_inc_wk, aes(x = time, y = death_rate, group = lk)) + geom_line() + theme_classic()
-grid.arrange(p1, p2, ncol = 1)
-
 p1 <- ggplot(data = dat_inc_cdp, aes(x = time, y = case_rate, group = lk)) + geom_line() + theme_classic()
 p2 <- ggplot(data = dat_inc_cdp, aes(x = time, y = death_rate, group = lk)) + geom_line() + theme_classic()
 grid.arrange(p1, p2, ncol = 1)
 
 # Sum over LKs:
-dat_inc_wk <- dat_inc_wk %>%
-  group_by(time) %>%
-  summarise(deaths = sum(deaths, na.rm = TRUE),
-            cases = sum(cases, na.rm = TRUE),
-            pop = sum(pop)) %>%
-  mutate(case_rate = cases / pop * 100000,
-         death_rate = deaths / pop * 1000000)
-
 dat_inc_cdp <- dat_inc_cdp %>%
   group_by(time) %>%
   summarise(deaths = sum(deaths),
@@ -56,32 +39,15 @@ dat_inc_cdp <- dat_inc_cdp %>%
          death_rate = deaths / pop * 1000000)
 
 # Plot:
-dat_inc_plot <- dat_inc_wk %>%
-  select(time, case_rate:death_rate) %>%
-  pivot_longer(-time, names_to = 'metric', values_to = 'val')
 dat_inc_plot_cdp <- dat_inc_cdp %>%
   select(time, case_rate:death_rate) %>%
   pivot_longer(-time, names_to = 'metric', values_to = 'val')
 
-p1 <- ggplot(data = dat_inc_plot, aes(x = time, y = val, color = metric)) +
+p3 <- ggplot(data = dat_inc_plot_cdp, aes(x = time, y = val, color = metric)) +
   geom_line() + scale_color_brewer(palette = 'Set1') + theme_classic()
-p2 <- ggplot(data = dat_inc_plot_cdp, aes(x = time, y = val, color = metric)) +
-  geom_line() + scale_color_brewer(palette = 'Set1') + theme_classic()
-grid.arrange(p1, p2, ncol = 1)
+print(p3)
 
 # Calculate lag:
-pks_cases <- which(
-  sapply(1:length(dat_inc_wk$time), function(ix) {
-    dat_inc_wk$case_rate[ix] > dat_inc_wk$case_rate[ix - 1] &
-      dat_inc_wk$case_rate[ix] > dat_inc_wk$case_rate[ix + 1]
-  }) == TRUE)
-
-pks_deaths <- which(
-  sapply(1:length(dat_inc_wk$time), function(ix) {
-    dat_inc_wk$death_rate[ix] > dat_inc_wk$death_rate[ix - 1] &
-      dat_inc_wk$death_rate[ix] > dat_inc_wk$death_rate[ix + 1]
-  }) == TRUE)
-
 pks_cases_cdp <- which(
   sapply(1:length(dat_inc_cdp$time), function(ix) {
     dat_inc_cdp$case_rate[ix] > dat_inc_cdp$case_rate[ix - 1] &
@@ -94,15 +60,11 @@ pks_deaths_cdp <- which(
       dat_inc_cdp$death_rate[ix] > dat_inc_cdp$death_rate[ix + 1]
   }) == TRUE)
 
-p1 <- ggplot(data = dat_inc_plot, aes(x = time, y = val, color = metric)) +
+p4 <- ggplot(data = dat_inc_plot_cdp, aes(x = time, y = val, color = metric)) +
   geom_line() + scale_color_brewer(palette = 'Set1') + theme_classic() +
-  geom_vline(xintercept = pks_cases + 12) +
-  geom_vline(xintercept = pks_deaths + 12, lty = 2)
-p2 <- ggplot(data = dat_inc_plot_cdp, aes(x = time, y = val, color = metric)) +
-  geom_line() + scale_color_brewer(palette = 'Set1') + theme_classic() +
-  geom_vline(xintercept = pks_cases_cdp + 9) +
-  geom_vline(xintercept = pks_deaths_cdp + 9, lty = 2)
-grid.arrange(p1, p2, ncol = 1)
+  geom_vline(xintercept = pks_cases_cdp + 8) +
+  geom_vline(xintercept = pks_deaths_cdp + 8, lty = 2)
+print(p4)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
--- src/explore_methods/explore_spatial_possibilities.R
+++ src/explore_methods/explore_spatial_possibilities.R
@@ -11,33 +11,14 @@ library(ggeffects)
 library(sf)
 library(testthat)
 library(spdep)
+library(viridis)
+library(gridExtra)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 # Read in and format data
+keep_map <- TRUE
 source('src/functions/load_data.R')
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-# Get map data and relevant coordinates
-
-# Read in map data:
-map_base <- st_read(dsn = 'data/raw/map/vg2500_01-01.gk3.shape/vg2500/vg2500_krs.shp')
-
-expect_true(all(unique(map_base$ARS) %in% unique(dat_inc$lk)))
-expect_true(all(unique(dat_inc$lk) %in% unique(map_base$ARS)))
-
-# Get neighborhood info:
-nb <- spdep::poly2nb(map_base, row.names = map_base$ARS)
-attr(nb, 'region.id') <- map_base$ARS
-names(nb) <- attr(nb, 'region.id')
-
-# Get CENTROID coordinates:
-map_base[, c('long', 'lat')] <- st_centroid(map_base) %>% st_transform(., '+proj=longlat') %>% st_coordinates()
-# Note: eventually probably want to do population center and not centroid - centroids aren't necessarily within LK
-
-# map_cent <- st_centroid(map_base)
-# ggplot() + geom_sf(data = map_base) + geom_sf(data = map_cent) + theme_void()
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -69,126 +50,56 @@ hist(box_ranges$xrange, xlab = 'Longitude Range')
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-# Can models be fit where lat/long are allowed to change over time? (Potentially relevant for travel data)
-
-# Add lat/long to deaths/cases data frame:
-dat_inc <- dat_inc %>%
-  left_join(map_base[, c('ARS', 'long', 'lat')],
-            by = 'ARS') %>%
-  select(-geometry)
-
-# Get number of regions and weeks:
-print(length(unique(dat_inc$ARS)))
-print(length(unique(dat_inc$Week)))
-
-# Need Bundesland as factor:
-dat_inc <- dat_inc %>%
-  mutate(bundesland = factor(bundesland))
-
-# Get specific data frame for deaths/cases analysis, where no cases == 0:
-dat_inc_fromCases <- dat_inc %>%
-  filter(cases > 0)
-
-# First, fit with constant lat/long:
-n1b <- bam(deaths ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 401) + s(Week, k = 62) + s(bundesland, bs = 're', k = 16) + offset(log(cases)),
-           data = dat_inc_fromCases, family = 'nb', method = 'fREML', nthreads = 4, discrete = TRUE)
-
-plot(n1b, pages = 1, scheme = 2, shade = TRUE, scale = 0)
-
-par(mfrow = c(2, 2))
-gam.check(n1b)
-
-# Now try allowing them to change:
-dat_inc_fromCases_NOISE <- dat_inc_fromCases %>%
-  mutate(long = long + rnorm(dim(dat_inc_fromCases)[1], mean = 0, sd = 0.5),
-         lat = lat + rnorm(dim(dat_inc_fromCases)[1], mean = 0, sd = 0.5))
-
-n1b.noise <- bam(deaths ~ s(long, lat, bs = 'ds', m = c(1.0, 0.5), k = 401) + s(Week, k = 62) + s(bundesland, bs = 're', k = 16) + offset(log(cases)),
-                 data = dat_inc_fromCases_NOISE, family = 'nb', method = 'fREML', nthreads = 4, discrete = TRUE)
-
-plot(n1b.noise, pages = 1, scheme = 2, shade = TRUE, scale = 0)
-
-par(mfrow = c(2, 2))
-gam.check(n1b.noise)
-# possible
-
-n1b.pred <- ggpredict(n1b)
-n1b.pred.noise <- ggpredict(n1b.noise, 'Week')
-# can't do ggpredict on lat or long anymore? Why is this?; Maybe interaction doesn't make sense anymore
-
-plot(n1b.pred$Week)
-plot(n1b.pred.noise)
-
-n1b.pred <- ggpredict(n1b, c('Week', 'lat'))
-n1b.pred.noise <- ggpredict(n1b.noise, c('Week', 'lat'))
-plot(n1b.pred)
-plot(n1b.pred.noise)
-
-# ---------------------------------------------------------------------------------------------------------------------
-
 # MRF weights based on travel?
 
-# Reload and format data:
-source('src/functions/load_data.R')
-
 # Need ARS and Bundesland as factor:
-dat_inc <- dat_inc %>%
-  mutate(ARS = factor(ARS),
+dat_cumulative <- dat_cumulative %>%
+  mutate(ARS = factor(lk),
          bundesland = factor(bundesland))
 
-# Get specific data frame for deaths/cases analysis, where no cases == 0:
-dat_inc_fromCases <- dat_inc %>%
-  filter(cases > 0)
-
 # Fit w/ default weights:
-m1b <- bam(deaths ~ s(ARS, bs = 'mrf', xt = list(nb = nb), k = 401) + s(Week, k = 62) + s(bundesland, bs = 're', k = 16) + offset(log(cases)),
-           data = dat_inc_fromCases, family = 'nb', method = 'fREML', nthreads = 4, discrete = TRUE)
-
-plot(m1b, pages = 1, scheme = 2, shade = TRUE, scale = 0)
+n2a <- gam(cases_wave2 ~ s(ARS, bs = 'mrf', xt = list(nb = nb), k = 300) + s(ags2, bs = 're', k = 16) +
+                    offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
 
 # Now try with differing weights by area:
 p_mat <- matrix(0, nrow = 401, ncol = 401)
 rownames(p_mat) = colnames(p_mat) = map_base$ARS
 diag(p_mat) <- card(nb)
-for (i in 1:96) {
+for (i in 1:401) {
   for (j in nb[i][[1]]) {
     p_mat[i, j] <- -0.5
-    # p_mat[j, i] <- -1
   }
 }
+n2a_1 <- gam(cases_wave2 ~ s(ARS, bs = 'mrf', xt = list(penalty = p_mat), k = 300) + s(ags2, bs = 're', k = 16) +
+             offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
 
-m2b <- bam(deaths ~ s(ARS, bs = 'mrf', xt = list(penalty = p_mat), k = 401) + s(Week, k = 62) + s(bundesland, bs = 're', k = 16) + offset(log(cases)),
-           data = dat_inc_fromCases, family = 'nb', method = 'fREML', nthreads = 4, discrete = TRUE)
-
-for (i in 1:96) {
+for (i in 1:401) {
   for (j in nb[i][[1]]) {
     p_mat[i, j] <- 0.5
   }
 }
+n2a_2 <- gam(cases_wave2 ~ s(ARS, bs = 'mrf', xt = list(penalty = p_mat), k = 300) + s(ags2, bs = 're', k = 16) +
+               offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
 
-m3b <- bam(deaths ~ s(ARS, bs = 'mrf', xt = list(penalty = p_mat), k = 401) + s(Week, k = 62) + s(bundesland, bs = 're', k = 16) + offset(log(cases)),
-           data = dat_inc_fromCases, family = 'nb', method = 'fREML', nthreads = 4, discrete = TRUE)
-
-for (i in 1:96) {
+for (i in 1:401) {
   for (j in nb[i][[1]]) {
     p_mat[i, j] <- 1
   }
 }
-
-m4b <- bam(deaths ~ s(ARS, bs = 'mrf', xt = list(penalty = p_mat), k = 401) + s(Week, k = 62) + s(bundesland, bs = 're', k = 16) + offset(log(cases)),
-           data = dat_inc_fromCases, family = 'nb', method = 'fREML', nthreads = 4, discrete = TRUE)
+n2a_3 <- gam(cases_wave2 ~ s(ARS, bs = 'mrf', xt = list(penalty = p_mat), k = 300) + s(ags2, bs = 're', k = 16) +
+               offset(log(pop)), data = dat_cumulative, family = 'nb', method = 'ML')
 # seems like values have to be between -1 and 1
 
-pdata <- with(dat_inc_fromCases,
-              expand.grid(cases = 100,
-                          bundesland = 'Bayern',
-                          ARS = ARS,
-                          Week = 56))
+# Plot predictions from each model:
+pdata <- with(dat_cumulative,
+              expand.grid(pop = 10000,
+                          ags2 = '09',
+                          ARS = ARS))
 
-m1.fit <- predict(m1b, pdata)
-m2.fit <- predict(m2b, pdata)
-m3.fit <- predict(m3b, pdata)
-m4.fit <- predict(m4b, pdata)
+m1.fit <- predict(n2a, pdata)
+m2.fit <- predict(n2a_1, pdata)
+m3.fit <- predict(n2a_2, pdata)
+m4.fit <- predict(n2a_3, pdata)
 
 m1.pred <- cbind(pdata, fitted = m1.fit)
 m2.pred <- cbind(pdata, fitted = m2.fit)
